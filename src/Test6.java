0c3936aa618f10b6b5992941a37cc3c1820d49c6
abhishek <abhishekanu.sharma7@gmail.com>
2022-02-13 22:09:13 +0530

    hi

--- src/Test6.java
+++ src/Test6.java
@@ -5,6 +5,9 @@ public class Test6 {
 
 		System.out.println("This is Test6 file");
 		System.out.println("This is test6 file by abhishek");
+		System.out.println("haridwar");
+		System.out.println("Meerut");
+		System.out.println("Delhi");
 	}
 
 }
